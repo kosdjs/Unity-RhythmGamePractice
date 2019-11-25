468ec0c53d6297eae2c3f441b75ac38af10133e5
kosdjs <kani39@naver.com>
2019-11-25 20:54:02 +0900

    순위 목록 표 구현

--- RhythmGame/Assets/Scripts/GameResultManager.cs
+++ RhythmGame/Assets/Scripts/GameResultManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 using System.IO;
 using System;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
 
 public class GameResultManager : MonoBehaviour
 {
@@ -12,6 +15,10 @@ public class GameResultManager : MonoBehaviour
     public Text scoreUI;
     public Text maxComboUI;
     public Image RankUI;
+
+    public Text rank1UI;
+    public Text rank2UI;
+    public Text rank3UI;
 
     // Start is called before the first frame update
     void Start()
@@ -43,6 +50,42 @@ public class GameResultManager : MonoBehaviour
         {
             RankUI.sprite = Resources.Load<Sprite>("Sprites/Rank C");
         }
+        rank1UI.text = "데이터를 불러오는 중입니다.";
+        rank2UI.text = "데이터를 불러오는 중입니다.";
+        rank3UI.text = "데이터를 불러오는 중입니다.";
+        DatabaseReference reference;
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://unity-rhythm-game-practice.firebaseio.com/");
+        reference = FirebaseDatabase.DefaultInstance.GetReference("ranks").Child(PlayerInformation.selectedMusic);
+        reference.OrderByChild("score").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCompleted)
+            {
+                List<String> rankList = new List<String>();
+                List<String> emailList = new List<String>();
+                DataSnapshot snapshot = task.Result;
+                foreach(DataSnapshot data in snapshot.Children)
+                {
+                    IDictionary rank = (IDictionary)data.Value;
+                    emailList.Add(rank["email"].ToString());
+                    rankList.Add(rank["score"].ToString());
+                }
+                emailList.Reverse();
+                rankList.Reverse();
+                rank1UI.text = "플레이 한 사용자가 없습니다.";
+                rank2UI.text = "플레이 한 사용자가 없습니다.";
+                rank3UI.text = "플레이 한 사용자가 없습니다.";
+                List<Text> textlist = new List<Text>();
+                textlist.Add(rank1UI);
+                textlist.Add(rank2UI);
+                textlist.Add(rank3UI);
+                int count = 1;
+                for(int i=0; i < rankList.Count && i < 3; i++)
+                {
+                    textlist[i].text = count + "위" + emailList[i] + " (" + rankList[i] + " 점)";
+                    count++;
+                }
+            }
+        });
     }
 
     public void Replay()
